b0b273192738893afecf725863824ca406b855dd
DianRuth1208 <dianaruthsantos@gmail.com>
2019-09-15 09:55:13 -0500

    2da version

--- Nodos.cpp
+++ Nodos.cpp
@@ -10,6 +10,18 @@ struct nodo {
 };
 
 int main (int argc, char** argv){
+	
+	
+	struct nodo *inicio;
+	struct nodo *aux;
+	inicio=NULL;
+	//IMPRIMIR EL VALOR DE UN NODO
+	//aux=new nodo ();
+	//aux->valor=25;
+	//aux->puntero=NULL;
+	//cout<<aux->valor<<endl;
+	
+	//IMPRIMIR NODOS CON DATOS
 	struct nodo dato1, dato2, dato3, dato4, dato5;
 	dato1.valor=3;
 	dato1.puntero=NULL;
@@ -19,7 +31,7 @@ int main (int argc, char** argv){
 	dato3.puntero=NULL;
 	dato4.valor=10;
 	dato4.puntero=NULL;
-	dato5.valor=12;
+	dato5.valor=11;
 	dato5.puntero=NULL;
 	
 	
@@ -28,14 +40,50 @@ int main (int argc, char** argv){
 	dato3.puntero=&dato4;
 	dato4.puntero=&dato5;
 	dato5.puntero=NULL;
-
+	
 	cout<<"\t"<<"VALOR DE NODOS"<<"\t"<<endl;
 	cout<<dato1.valor<<endl;
 	cout<<dato1.puntero->valor<<endl;
 	cout<<dato1.puntero->puntero->valor<<endl;
 	cout<<dato1.puntero->puntero->puntero->valor<<endl;
 	cout<<dato1.puntero->puntero->puntero->puntero->valor<<endl;
+	
+	
+	//IMPRIMIR NODOS CON EL AUXILIAR
+	aux=new nodo();
+	aux->valor=12;
+	aux->puntero=NULL;
+	inicio=aux;
+	
+	aux=new nodo();
+	aux->valor=13;
+	aux->puntero=NULL;
+	inicio->puntero=aux;
+	
+	aux=new nodo();
+	aux->valor=14;
+	aux->puntero=NULL;
+	inicio->puntero->puntero=aux;
+	
+	aux=new nodo();
+	aux->valor=15;
+	aux->puntero=NULL;
+	inicio->puntero->puntero->puntero=aux;
+	
+	aux=new nodo();
+	aux->valor=16;
+	aux->puntero=NULL;
+	inicio->puntero->puntero->puntero->puntero=aux;
+	
+	cout<<"\t"<<"VALOR DE NODOS"<<"\t"<<endl;
+	cout<<inicio->valor<<endl;
+	cout<<inicio->puntero->valor<<endl;
+	cout<<inicio->puntero->puntero->valor<<endl;
+	cout<<inicio->puntero->puntero->puntero->valor<<endl;
+	cout<<inicio->puntero->puntero->puntero->puntero->valor<<endl;
 
+
+	
 	getch();
 	return 0;
 	
